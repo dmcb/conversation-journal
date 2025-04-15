8dd8757228bb5d3451c4775bfe4daaea6ca176d0
dmcb <d.mcburney@icloud.com>
2025-04-14 20:51:19 -0600

    Adds show more button

--- src/lib/components/EntriesList.svelte
+++ src/lib/components/EntriesList.svelte
@@ -6,15 +6,28 @@
 	}
 
 	export let entries: Entry[] = [];
+	let displayState = 'default'; // 'default' | 'more' | 'all'
+	const defaultEntries = 5;
+	const moreEntries = 35;
+
+	$: displayedEntries =
+		displayState === 'all'
+			? entries
+			: displayState === 'more'
+				? entries.slice(0, moreEntries)
+				: entries.slice(0, defaultEntries);
+	$: remainingEntries = entries.length - moreEntries;
+	$: hasMoreEntries = entries.length > defaultEntries;
+	$: hasExcessEntries = entries.length > moreEntries;
 </script>
 
 <section class="entries">
-	<h2>Talked recently with</h2>
+	<h2>Talked recently with:</h2>
 	{#if entries.length === 0}
-		<p>No names added yet</p>
+		<p>No names added yet.</p>
 	{:else}
 		<ul>
-			{#each entries as entry}
+			{#each displayedEntries as entry}
 				<li>
 					<div class="entry-header">
 						<span class="name">{entry.name}</span>
@@ -29,6 +42,14 @@
 				</li>
 			{/each}
 		</ul>
+		{#if hasMoreEntries && displayState === 'default'}
+			<button class="show-more" on:click={() => (displayState = 'more')}>Show More</button>
+		{:else if hasExcessEntries && displayState === 'more'}
+			<button class="show-more" on:click={() => (displayState = 'all')}>
+				Show remaining {remainingEntries}
+				{remainingEntries === 1 ? 'contact' : 'contacts'}
+			</button>
+		{/if}
 	{/if}
 </section>
 
@@ -60,5 +81,21 @@
 
 	.days {
 		color: #666;
+	}
+
+	.show-more {
+		cursor: pointer;
+		background-color: transparent;
+		border: none;
+		font-weight: bold;
+		color: var(--color4, #a25b9f);
+		transition: all 0.3s ease;
+		display: block;
+		margin: 0 auto;
+		padding: 0;
+	}
+
+	.show-more:hover {
+		color: color-mix(in srgb, var(--color4, #a25b9f) 80%, black);
 	}
 </style>

94080f68414f7dabe86f21221a3b37c434f3e226
dmcb <d.mcburney@icloud.com>
2025-05-03 21:26:27 -0600

    Tweaks to intro page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,62 +60,62 @@
 
 	<section class="features">
 		<ul>
-		<li>
-			<svg
-				class="icon"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
-				<path d="M13.73 21a2 2 0 0 1-3.46 0" />
-			</svg>
-			<div class="text">
-				<span>Get reminders</span>
-				<p>You'll be recommended who to reconnect with</p>
-			</div>
-		</li>
-		<li>
-			<svg
-				class="icon"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
-				<line x1="16" y1="2" x2="16" y2="6" />
-				<line x1="8" y1="2" x2="8" y2="6" />
-				<line x1="3" y1="10" x2="21" y2="10" />
-			</svg>
-			<div class="text">
-				<span>See who matters</span>
-				<p>Get visuals on who you talk to and when</p>
-			</div>
-		</li>
-		<li>
-			<svg
-				class="icon"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<path d="M12 20h9" />
-				<path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" />
-			</svg>
-			<div class="text">
-				<span>Record memories</span>
-				<p>Quickly log your conversations</p>
-			</div>
-		</li>
+			<li>
+				<svg
+					class="icon"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path d="M12 20h9" />
+					<path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" />
+				</svg>
+				<div class="text">
+					<span>Record memories</span>
+					<p>Quickly log chats to your journal</p>
+				</div>
+			</li>
+			<li>
+				<svg
+					class="icon"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+					<line x1="16" y1="2" x2="16" y2="6" />
+					<line x1="8" y1="2" x2="8" y2="6" />
+					<line x1="3" y1="10" x2="21" y2="10" />
+				</svg>
+				<div class="text">
+					<span>See who matters</span>
+					<p>Get visuals on who you talk to and when</p>
+				</div>
+			</li>
+			<li>
+				<svg
+					class="icon"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+					<path d="M13.73 21a2 2 0 0 1-3.46 0" />
+				</svg>
+				<div class="text">
+					<span>Get reminders</span>
+					<p>Get notified when it's time to reconnect</p>
+				</div>
+			</li>
 		</ul>
 	</section>
 {/if}
@@ -175,6 +175,10 @@
 
 	li span {
 		font-weight: 800;
+	}
+
+	li p {
+		margin-bottom: 0;
 	}
 
 	.first-entry h2 {
